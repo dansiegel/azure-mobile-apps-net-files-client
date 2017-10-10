57ef1926b8d5fa5888765d70d0dd0d0b6feb9cfe
Dan Siegel <me@dansiegel.net>
2017-10-10 11:50:03 -0700

    merging Table and SyncTable Extensions

--- src/Microsoft.WindowsAzure.Mobile.Files/Sync/MobileServiceSyncTableExtensions.cs
+++ src/Microsoft.WindowsAzure.Mobile.Files/Sync/MobileServiceSyncTableExtensions.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Files.IO;
 using Microsoft.WindowsAzure.MobileServices.Files.Metadata;
 using Microsoft.WindowsAzure.MobileServices.Files.Sync;
 using Microsoft.WindowsAzure.MobileServices.Sync;
 
 namespace Microsoft.WindowsAzure.MobileServices.Files
 {
-    public static class MobileServiceSyncTableExtensions
+    public static class MobileServiceSyncTableExtensionsfoo
     {
         private static IFileSyncHandler fileSyncHandler;
         public async static Task<IEnumerable<MobileServiceFile>> GetFilesAsync<T>(this IMobileServiceSyncTable<T> table, T dataItem)
@@ -89,7 +91,26 @@ namespace Microsoft.WindowsAzure.MobileServices.Files
             await context.AddFileAsync(file);
         }
 
-     
+        public async static Task UploadFileAsync<T>(this IMobileServiceSyncTable<T> table, MobileServiceFile file, string filePath)
+        {
+            IMobileServiceFileDataSource dataSource = new PathMobileServiceFileDataSource(filePath);
+
+            MobileServiceFileMetadata metadata = MobileServiceFileMetadata.FromFile(file);
+
+            IFileSyncContext context = table.MobileServiceClient.GetFileSyncContext();
+
+            await context.MobileServiceFilesClient.UploadFileAsync(metadata, dataSource);
+        }
+
+        public async static Task DownloadFileAsync<T>(this IMobileServiceSyncTable<T> table, MobileServiceFile file, string targetPath)
+        {
+            using (Stream stream = await Mobile.Files.IO.File.CreateAsync(targetPath))
+            {
+                IFileSyncContext context = table.MobileServiceClient.GetFileSyncContext();
+
+                await context.MobileServiceFilesClient.DownloadToStreamAsync(file, stream);
+            }
+        }
 
         public async static Task DeleteFileAsync<T>(this IMobileServiceSyncTable<T> table, MobileServiceFile file)
         {
